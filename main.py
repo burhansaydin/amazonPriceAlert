659afdf30b55743af3f0fea98d04fc954aee437b
burhansaydin <burhasaydin@gmail.com>
2021-01-14 23:32:44 +0200

    -smtplib is imported in order to send mail to designated email address when the price is below the intended price -stated price in the amazon site is converted float to compare the prices -needed codes are written in order to send mail.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import smtplib
+
 import requests
 from bs4 import BeautifulSoup
 import lxml
@@ -7,6 +9,7 @@ url="https://www.amazon.com/Apple-16-MacBook-Pro-Z0XZ0007A/dp/B08374BBQG/ref=sr_
 TARGET_PRICE = 3000
 from_mail = "abcde@gmail.com"
 to_mail = "burhansaydin@gmail.com"
+password = "asdfsadfadsf"
 headers = {
     "Accept-Language": "en-US,en;q=0.9",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
@@ -17,6 +20,23 @@ response = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(response.text, "lxml")
 
-price = soup.find("span", id="priceblock_ourprice", class_="a-size-medium a-color-price priceBlockBuyingPriceString")
+price = soup.find("span", id="priceblock_ourprice", class_="a-size-medium a-color-price priceBlockBuyingPriceString").getText()
+title = soup.find("span", id="productTitle", class_="a-size-large product-title-word-break").getText()
 
-print(price.getText())
+price_str = price.split("$")[1]
+last_price = price_str.split(",")
+gathered_str = last_price[0]+last_price[1]
+price_float = float(gathered_str)
+
+if price_float < TARGET_PRICE:
+
+    message = f"{title} is now {price_float}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(from_mail, password)
+        connection.sendmail(
+            from_addr=from_mail,
+            to_addrs=to_mail,
+            msg=f"Subject: Amazon Price Alert!\n\n{message}\n{url}"
+        )
